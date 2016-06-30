0e113630ff7a0442c74afc58ab3ba1684e7fb510
Daniel Douangkesone <karateporkchop@gmail.com>
2016-06-30 18:48:10 -0500

    first homework assignment

---
+++ 02-data-types/magic-8-ball.js
@@ -0,0 +1,4 @@
+var results = ['It is certain', 'It is decidedly so.', 'Without a doubt', 'Yes, definitely.', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good' ];
+var x = Math.floor(Math.random() * (8 - 1 + 1)) + 1;
+
+console.log(results[x]);
